1ba2d4cb011a3521fe42f2c500f89ca8aeff41fb
Chaika-Dilshan-Anuruddha <anuruddhaedcd@gmail.com>
2024-03-01 16:48:19 +0530

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,156 @@
+1. create ubuntu 2 GB ram, 20 gb hard ec2
+
+
+2. Install MySQL Server on your Ubuntu instance:
+sudo apt update
+sudo apt install mysql-server
+
+3. Log in to MySQL:
+sudo su
+mysql -u root -p
+give a password  (12345)
+
+4. Create a database and user, and grant privileges:
+CREATE DATABASE broodbox;
+CREATE USER 'dilshan'@'localhost' IDENTIFIED BY '1234'; 
+GRANT ALL PRIVILEGES ON broodbox.* TO 'dilshan'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+(here 1234 the password of the user)
+
+5.create table
+USE broodbox;
+CREATE TABLE `farm_details` (
+ `business_id` varchar(255), `farm_id` varchar(255) PRIMARY KEY, `boundaries` varchar(255));
+
+6.insert values
+INSERT INTO `farm_details` (`business_id`, `farm_id`, `boundaries`)
+VALUES
+('B456', '123', '[(123,344),(434.3,34.5),(434.3,34.5),(434.3,34.5)(434.3,34.5)(434.3,34.5)(434.3,34.5)(434.3,34.5)(434.3,34.5),(434.3,34.5)]'),
+('B678', '456', 'Boundary 2'),
+('B999', '78t', 'Boundary 3');
+
+7.create table
+CREATE TABLE `hive_details_B456_123` (
+ `area_code` varchar(20), `location_code` varchar(20),  `longitude` double, 
+`latitude` double, `total_beehives` int(20), `total_active_frames` int(20),
+PRIMARY KEY (`area_code`, `location_code`));
+
+
+8. clone the git repo
+open new terminal 
+git clone https://github.com/Chamika-ML/database-api3.git
+
+
+9. insert hvie_details.csv file data to table
+
+go to first
+a) SHOW VARIABLES LIKE "secure_file_priv";
+   copy  the valu path ( /var/lib/mysql-files/ )
+
+go to second terminal
+b) sudo mv /home/ubuntu/database-api3/hive_details.csv /var/lib/mysql-files/
+
+go to first terminal
+c) LOAD DATA INFILE '/var/lib/mysql-files/hive_details.csv'
+   INTO TABLE `hive_details_B456_123`
+   FIELDS TERMINATED BY ','
+   OPTIONALLY ENCLOSED BY '"'
+   LINES TERMINATED BY '\n'
+   IGNORE 1 LINES;
+
+d)SELECT * FROM `hive_details_B456_123`; 
+
+10. change the line main.py
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dilshan:1234@localhost/broodbox'
+
+
+11. make virtual env for run api
+
+go to second terminal
+cd database-api3
+sudo apt-get install python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+if tere are (MYSQLdb install errors, then run below):
+sudo apt-get install -y libmysqlclient-dev pkg-config
+sudo apt-get install build-essential
+sudo apt-get install python3-dev
+
+
+
+12.Run 
+gunicorn -w 4 -b 0.0.0.0:5000 main:app
+
+13. test the api:
+
+insall thunder client to the vs code
+
+GET
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/farm
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/farm/123
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/B456/123
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/area-location-codes/B456/123
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/get/B456/123/1/11139 
+
+
+POST
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/farm/add
+{ 
+  "business_id":"asd12",
+  "farm_id":343,
+  "boundaries":"boundry 12445"
+}
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/farm/delete/343
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/farm/edit/456
+
+{
+  "farm_id":456,
+  "boundaries":"This is the new boundary for id 456"
+}
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/add/B456/123 
+
+{"data": [{
+  "area_code":987654,
+  "location_code":12345,
+  "longitude":12.3456,
+  "latitude":-123.4567,
+  "total_beehives":1000
+},
+{"area_code":321456,
+  "location_code":67534,
+  "longitude":22.3456,
+  "latitude":-563.4567,
+  "total_beehives":400
+}
+]}
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/update/B456/123/987654/12345
+
+{
+  "area_code":"This is the new",
+  "location_code":54321,
+  "longitude":1212.3456,
+  "latitude":6123.4567,
+  "total_beehives":15000   
+}
+
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/delete/B456/123/321456/67534
+* http://ec2-54-206-119-102.ap-southeast-2.compute.amazonaws.com:5000/hive/delete/B456/123/This is the new/54321
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,313 @@
+from flask import Flask, jsonify, request, json
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow 
+from sqlalchemy import PrimaryKeyConstraint, text, desc, asc, cast, Integer
+from flask import request
+from math import ceil
+
+app = Flask(__name__)
+db = SQLAlchemy()
+ma = Marshmallow()
+
+# Farm details class
+class Farm(db.Model):
+    __tablename__ = 'farm_details'
+    business_id = db.Column(db.String(255), autoincrement=False, nullable=False)
+    farm_id = db.Column(db.String(255), primary_key=True, autoincrement=False)
+    boundaries = db.Column(db.String(255), nullable=False)
+
+
+    def __init__(self, business_id, farm_id, boundaries):
+        self.business_id = business_id
+        self.farm_id = farm_id
+        self.boundaries = boundaries
+
+class FarmSchema(ma.Schema):
+    class Meta:
+        fields = ('business_id', 'farm_id', 'boundaries')
+
+Farm_schema = FarmSchema()
+Farms_schema = FarmSchema(many=True)
+
+# Hive details class
+# Global dictionary to store dynamically created Hive classes
+dynamic_hive_classes = {}
+
+# Hive details class
+def create_hive_class(business_id, farm_id):
+    table_name = f"hive_details_{business_id}_{farm_id}"
+    
+    # Check if the Hive class for this table already exists
+    if table_name in dynamic_hive_classes:
+        Hive = dynamic_hive_classes[table_name]
+    else:
+        class Hive(db.Model): 
+            __tablename__ = table_name
+            
+            area_code = db.Column(db.String(30), nullable=False)
+            location_code = db.Column(db.String(30), nullable=False)
+            longitude = db.Column(db.Float, nullable=False)
+            latitude = db.Column(db.Float, nullable=False)
+            total_beehives = db.Column(db.Integer, nullable=False)
+            total_active_frames = db.Column(db.Integer, nullable=True)
+
+            __table_args__ = (
+                PrimaryKeyConstraint('area_code', 'location_code'),
+            )
+
+            def __init__(self, area_code, location_code, longitude, latitude, total_beehives, total_active_frames):
+                self.area_code = area_code
+                self.location_code = location_code
+                self.longitude = longitude
+                self.latitude = latitude
+                self.total_beehives = total_beehives
+                self.total_active_frames = total_active_frames
+
+        # Save the class in the global dictionary
+        dynamic_hive_classes[table_name] = Hive
+
+    return Hive
+
+
+class HiveSchema(ma.Schema):
+    class Meta:
+        fields = ('area_code','location_code','longitude','latitude','total_beehives','Total_Active_frames')
+
+Hive_schema = HiveSchema()
+Hives_schema  = HiveSchema(many=True)
+
+
+# MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dilshan:1234@localhost/broodbox'
+db.init_app(app)
+
+# Farm details table related  apis
+
+@app.route('/farm/add', methods=['POST'])
+def add_Farm():
+    _json = request.json
+    business_id = _json['business_id']
+    farm_id = _json['farm_id']
+    boundaries = _json['boundaries']
+
+    new_Farm = Farm(business_id=business_id, farm_id=farm_id, boundaries=boundaries)
+    db.session.add(new_Farm)
+    db.session.commit()
+
+    # Check if the corresponding hive_details table already exists
+    hive_table_name = f"hive_details_{business_id}_{farm_id}"
+    if hive_table_name not in db.metadata.tables:
+        # If the table doesn't exist, create it
+        Hive = create_hive_class(business_id, farm_id)
+        db.create_all()
+    
+    return jsonify({"message": "the Farm has been added "})
+
+
+@app.route('/farm', methods=['GET'])
+def get_Farm():
+    Farms = []
+    data = Farm.query.all()
+    Farms = Farms_schema.dump(data)
+    return jsonify(Farms)
+
+@app.route('/farm/<farm_id>', methods=['GET'])
+def Farm_byid(farm_id):
+    farm_record = Farm.query.get(farm_id)
+    data = Farm_schema.dump(farm_record)
+    return jsonify(data)
+
+
+@app.route('/farm/delete/<farm_id>', methods=['POST'])
+def delete_Farm(farm_id):
+    farm_record = Farm.query.get(farm_id)
+    if farm_record is None:
+        return jsonify(f"Error: the Farm doesn't exist")
+    
+    # Delete the farm record
+    db.session.delete(farm_record)
+    db.session.commit()
+
+    # Determine the name of the corresponding hive details table
+    hive_table_name = f"hive_details_{farm_record.business_id}_{farm_id}"
+
+    # Drop the corresponding hive details table
+    try:
+        db.session.execute(text(f"DROP TABLE IF EXISTS {hive_table_name}"))
+        db.session.commit()
+        return jsonify({"message": "the Farm has been deleted, along with its corresponding hive details table"})
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)})
+
+
+@app.route('/farm/edit/<farm_id>', methods=['POST'])
+def edit_Farm(farm_id):
+    farm_record = Farm.query.get(farm_id)
+    if farm_record is None:
+        return jsonify({"error": "the farm doesn't exist"})
+    _json = request.json
+    farm_record.boundaries = _json['boundaries']
+    db.session.commit()
+    return jsonify({"message": "the Farm has been edited"})
+
+
+# hive details table related apis
+
+@app.route('/hive/<business_id>/<farm_id>', methods=['GET'])
+def get_hive_details(business_id, farm_id):
+    # Use the dynamically created Hive class based on farm_id
+    Hive = create_hive_class(business_id, farm_id)
+    try:
+        # Extracting query parameters
+        limit = int(request.args.get('limit', 1000))  # Default limit is 1000 if not provided
+        page = int(request.args.get('page', 1))  # Default page is 1 if not provided
+        sort_by = request.args.get('sortBy', 'area_code:asc')  # Default sorting by area_code in acending order if not provided
+        search_term = request.args.get('search', '')  # Default empty search term if not provided
+        
+        # Split sort_by parameter into field and order
+        sort_field, sort_order = sort_by.split(':')
+
+        # Calculate offset for pagination
+        offset = (page - 1) * limit
+
+        # Retrieve records with optional sorting, search, and pagination
+        if search_term:
+            hive_records = Hive.query.filter(Hive.area_code == search_term).order_by(cast(getattr(Hive, sort_field), Integer).desc() if sort_order == 'desc' else cast(getattr(Hive, sort_field), Integer).asc()).offset(offset).limit(limit).all()
+        else:
+            hive_records = Hive.query.order_by(cast(getattr(Hive, sort_field), Integer).desc() if sort_order == 'desc' else cast(getattr(Hive, sort_field), Integer).asc()).offset(offset).limit(limit).all()
+
+        # Serialize the records using the HiveSchema
+        hive_details = Hives_schema.dump(hive_records)
+        
+        # Count total number of records
+        total_records = Hive.query.count()
+        
+        # Calculate total pages
+        total_pages = ceil(total_records / limit)
+
+        # Return paginated results along with pagination metadata
+        return jsonify({
+            "hive_details": hive_details,
+            "pagination": {
+                "page": page,
+                "limit": limit,
+                "total_pages": total_pages,
+                "total_records": total_records
+            }
+        })
+    
+    except:
+        return jsonify({"error": "the farm doesn't exist"})
+
+
+
+@app.route('/hive/get/<business_id>/<farm_id>/<area_code>/<location_code>', methods=['GET'])
+def get_single_hive(business_id, farm_id, area_code, location_code):
+    try:
+        Hive = create_hive_class(business_id, farm_id)
+        hive = Hive.query.filter_by(area_code=area_code, location_code=location_code).first()
+
+        if hive is None:
+            return jsonify({"error": "Hive not found"})
+
+        hive_data = Hive_schema.dump(hive)
+        return jsonify(hive_data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+@app.route('/hive/area-location-codes/<business_id>/<farm_id>', methods=['GET'])
+def get_location_codes(business_id, farm_id):
+    # Use the dynamically created Hive class based on farm_id
+    Hive = create_hive_class(business_id, farm_id)
+    
+    try:
+        # Retrieve all area_code values from the corresponding hive_details_xx table
+        area_codes = Hive.query.with_entities(Hive.area_code).distinct().all()
+
+        # Retrieve all location_code values from the corresponding hive_details_xx table
+        location_codes = Hive.query.with_entities(Hive.location_code).distinct().all()
+
+        # Convert the result to a list of area_code values
+        area_codes_list = [area_code[0] for area_code in area_codes]
+
+        # Convert the result to a list of location_code values
+        location_codes_list = [location_code[0] for location_code in location_codes]
+
+        return jsonify({"area_codes": area_codes_list, "location_codes": location_codes_list})
+    
+    except:
+        return jsonify({"error": "the farm doesn't exist"})
+
+
+
+@app.route('/hive/add/<business_id>/<farm_id>', methods=['POST'])
+def add_hive(business_id, farm_id):
+    try:
+        Hive = create_hive_class(business_id, farm_id)
+        _json = request.json.get('data', [])  # Get the list of hive data from the JSON, default to empty list if not provided
+
+        # Add hive details to the dynamically created hive table
+        for hive_item in _json:
+            new_hive = Hive(
+                area_code=hive_item['area_code'],
+                location_code=hive_item['location_code'],
+                longitude=hive_item['longitude'],
+                latitude=hive_item['latitude'],
+                total_beehives=hive_item['total_beehives'],
+                total_active_frames=None  # Set to None for nullable column
+            )
+
+            db.session.add(new_hive)
+
+        db.session.commit()
+
+        return jsonify({"message": "Hive(s) added successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+
+@app.route('/hive/update/<business_id>/<farm_id>/<area_code>/<location_code>', methods=['POST'])
+def update_hive(business_id, farm_id, area_code, location_code):
+    try:
+        Hive = create_hive_class(business_id, farm_id)
+        hive = Hive.query.filter_by(area_code=area_code, location_code=location_code).first()
+
+        if hive is None:
+            return jsonify({"error": "Hive not found"})
+
+        _json = request.json
+        hive.area_code = _json['area_code']
+        hive.location_code = _json['location_code']
+        hive.longitude = _json['longitude']
+        hive.latitude = _json['latitude']
+        hive.total_beehives = _json['total_beehives']
+
+        db.session.commit()
+
+        return jsonify({"message": "Hive updated successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+@app.route('/hive/delete/<business_id>/<farm_id>/<area_code>/<location_code>', methods=['POST'])
+def delete_hive(business_id, farm_id, area_code, location_code):
+    try:
+        Hive = create_hive_class(business_id, farm_id)
+        hive = Hive.query.filter_by(area_code=area_code, location_code=location_code).first()
+
+        if hive is None:
+            return jsonify({"error": "Hive not found"})
+
+        db.session.delete(hive)
+        db.session.commit()
+
+        return jsonify({"message": "Hive deleted successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
